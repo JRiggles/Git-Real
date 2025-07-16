48ac0fa060e31723a021b9a06232fe17e861f8d8
John Riggles <jriggles@icloud.com>
2025-07-15 21:43:09 -0400

    Update main.py
    Clean up imports,
    Fix/improve type hinting and fallback types,
    Create constants for WIDTH and HEIGHT to avoid repeated attr access
    Etc.

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
-import adafruit_ntp
-import board  # type: ignore (disregard import error - Pylance is wrong)
-import busio
 import os
-import rtc
-import socketpool
-import ssl
-import time
-import wifi
-from adafruit_is31fl3731.charlie_wing import CharlieWing
-from adafruit_requests import Session
 from random import randrange
+from ssl import create_default_context
+from time import sleep
+
+import board  # type: ignore
+from adafruit_is31fl3731.charlie_wing import CharlieWing  # type: ignore
+from adafruit_ntp import NTP  # type: ignore
+from adafruit_requests import Session  # type: ignore
+from busio import I2C  # type: ignore
+from digitalio import DigitalInOut  # type: ignore
+from rtc import RTC  # type: ignore
+from socketpool import SocketPool  # type: ignore
+from wifi import radio  # type: ignore
 
 # TODO: battery power? (+ power management)
 # IDEA: lower max_brightness when on battery power
 
 BASE_URL = 'https://github-contributions-api.deno.dev/'
 RESPONSE_TYPE = '.text?no-total=true'
-USERNAME = os.getenv('USERNAME')
-SSID = os.getenv('CIRCUITPY_WIFI_SSID')
-PWD = os.getenv('CIRCUITPY_WIFI_PASSWORD')
+USERNAME = os.getenv('USERNAME') or ''
+SSID = os.getenv('CIRCUITPY_WIFI_SSID') or ''
+PWD = os.getenv('CIRCUITPY_WIFI_PASSWORD') or ''
+
+# disable the neopixel to save power
+np_power = DigitalInOut(board.NEOPIXEL_POWER)
+np_power.switch_to_input()
 
 # https://learn.adafruit.com/adafruit-15x7-7x15-charlieplex-led-matrix-charliewing-featherwing
 
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = I2C(board.SCL, board.SDA)
 display = CharlieWing(i2c)
+WIDTH = display.width
+HEIGHT = display.height
 max_brightness = 64
 
 
 def connect() -> Session:
     """Connect to WiFi and return a `Session` object"""
-    wifi.radio.connect(SSID, PWD)
-    pool = socketpool.SocketPool(wifi.radio)
+    radio.connect(SSID, PWD)
+    pool = SocketPool(radio)
     # get the current time from a NTP server
-    ntp = adafruit_ntp.NTP(pool, server="pool.ntp.org", tz_offset=-4)
-    current_time = ntp.datetime
-    # update the system RTC
-    rtc.RTC().datetime = current_time
-    session = Session(pool, ssl.create_default_context())
+    ntp = NTP(pool, server="pool.ntp.org")
+    # update the system RTC to the current time
+    RTC().datetime = ntp.datetime
+    session = Session(pool, create_default_context())
     return session
 
 
@@ -44,31 +51,30 @@ def get_data(session) -> str:
     """Get GitHub contribution data"""
     response = session.get(BASE_URL + USERNAME + RESPONSE_TYPE)
     if (status := response.status_code) == 200:
-        return response.content
+        return response.content.decode()
     else:
         return str(status)
 
 
-def get_rows(data: str) -> list[map]:
+def get_rows(data: str) -> list[map[str]]:
     """Split up the contribution data into rows of values"""
     # split response content into rows, removing the empty cell at the end
-    rows = data.decode().split('\n')
-    # split the rows into maps of strings, keeping the last 'display.width'
-    # elements
-    string_maps = [
-        map(str.strip, row.split(',')[-display.width:]) for row in rows
-    ][:-1]  # remove empty element at the end
-    return string_maps
+    rows = data.split('\n')
+    cols = WIDTH
+    # split the rows into maps of strings, keeping the last 'cols' elements and
+    # removing empty element at the end
+    row_maps = [map(str.strip, row.split(',')[-cols:]) for row in rows][:-1]
+    return row_maps
 
 
-def normalize_data(string_maps: list[map]) -> list[int]:
+def normalize_values(row_maps: list[map[str]]) -> list[int]:
     """
-    Normalize the values in the contribution data string to a range of
+    Normalize the values in the contribution data maps to a range of
     `0` to `max_brightness` and return them as a list of `int`s
     """
     # convert string values to ints, replacing the empty strings with 0
     int_values = [
-        int(n) if n.isdigit() else 0 for row in string_maps for n in row
+        int(n) if n.isdigit() else 0 for row in row_maps for n in row
     ]
     # normalize the integer values to a range of 0 to max_brigtness
     peak = max(int_values)
@@ -79,15 +85,15 @@ def normalize_data(string_maps: list[map]) -> list[int]:
 def update_display(led_values: list[int]) -> None:
     """Set the LEDs according to the given values"""
     for index, value in enumerate(led_values):
-        y, x = divmod(index, display.width)
+        y, x = divmod(index, WIDTH)
         display.pixel(x, y, value)
 
 
 def randomize(frames: int) -> None:
     """Show a random array on the display for the number of `frames` given"""
     for _ in range(frames):
-        for pixel in range(display.width * display.height):
-            y, x = divmod(pixel, display.height)
+        for pixel in range(WIDTH * HEIGHT):
+            y, x = divmod(pixel, HEIGHT)
             brightness = randrange(0, max_brightness)
             if brightness % 2:  # skew towards 0
                 brightness = 0
@@ -98,14 +104,14 @@ def mainloop() -> None:
     session = connect()
     last_update_time = None
     while True:  # check for updates every hour
-        if (current_hour := rtc.RTC().datetime.tm_hour) != last_update_time:
+        if (current_hour := RTC().datetime.tm_hour) != last_update_time:
             data = get_data(session)
             rows = get_rows(data)
-            led_values = normalize_data(rows)
-            randomize(16)  # show refresh animation
+            led_values = normalize_values(rows)
+            randomize(8)  # show refresh animation
             update_display(led_values)
             last_update_time = current_hour
-        time.sleep(300)
+        sleep(300)  # sleep for 5 minutes before checking again to save power
 
 
 if __name__ == '__main__':
